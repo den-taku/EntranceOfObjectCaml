47685458ad78b3bdcf852ed583d1cebf0c326eb2
DenTaku <dentaku1412@gmail.com>
2020-09-28 16:43:46 +0900

    Implement "capitalize"

--- 2/functions.ml
+++ 2/functions.ml
@@ -7,3 +7,8 @@ let dollar_of_yen_f y =
 let yen_of_dollar d =
   floor( d *. 111.12 );;
 
+let capitalize c =
+  let c_int = int_of_char c in
+    if 97 <= c_int && c_int <= 122 then char_of_int (c_int - 32)
+    else char_of_int c_int
+;;
