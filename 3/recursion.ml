2d0c6e4f24694676ea1f44c5a45e005ab5dd9ea6
DenTaku <dentaku1412@gmail.com>
2020-09-29 20:09:06 +0900

    Learn evaluation strategy

--- 3/recursion.ml
+++ 3/recursion.ml
@@ -1,2 +1,17 @@
 let rec fact n = 
-  if n = 1 then 1 else fact (n-1) * n ;;
+  if n = 1 then 1 else fact (n-1) * n 
+;;
+
+let square x = x * x;;
+square(square(2));; (*call-by-value*)
+
+let rec facti (n, res) = 
+  if n = 1 then res 
+  else facti (n-1, n * res)
+;;
+
+let fact n = 
+  let rec facti (n, res) = 
+    if n = 1 then res else  facti(n - 1, res * n)
+  in facti(n, 1)
+;;
