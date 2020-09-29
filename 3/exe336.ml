d47af89d9b0920e86f36c9527943870ee76efaf3
DenTaku <dentaku1412@gmail.com>
2020-09-30 01:48:58 +0900

    Implement gcd

--- 3/exe336.ml
+++ 3/exe336.ml
@@ -5,7 +5,7 @@ and odd n =
 ;;
 
 let rec pow'(x, n) = (* x ^ n *)
-  if n = 1 then x else x * pow(x, n-1)
+  if n = 1 then x else x * pow'(x, n-1)
 ;;
 
 let rec pow(x, n) = (* x ^ n *)
@@ -17,3 +17,21 @@ let rec pow(x, n) = (* x ^ n *)
 let rec powi(x, n, res) = 
   if n = 0 then res else powi(x, n-1, x * res)
 ;;
+
+let gcd(n, m) = 
+  let rec inner_fn(n, m) = (* n >= m *) 
+    let d = n mod m in
+    if d = 0 then m 
+    else inner_fn(m, d)
+  in
+    if n >= m then inner_fn(n, m) 
+    else inner_fn(m, n)
+;;
+
+(* let rec gcd (n, m) = 
+  if n = m then n
+  else if n > m then 
+    if n mod m = 0 then m else gcd(m, n mod m)
+  else 
+    if m mod n = 0 then n else gcd(n, m mod n)
+;; *)
