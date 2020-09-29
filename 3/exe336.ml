50641fe9f5808c957d21e6c0f003d38c94409e5d
DenTaku <dentaku1412@gmail.com>
2020-09-30 00:58:40 +0900

    Reimplement pow

--- 3/exe336.ml
+++ 3/exe336.ml
@@ -1,3 +1,15 @@
-let rec pow(x, n) = (* x ^ n *)
+let rec even n = (* works for positive intergers *)
+  if n = 0 then true else odd(n-1)
+and odd n =
+  if n = 0 then false else even(n-1)
+;;
+
+let rec pow'(x, n) = (* x ^ n *)
   if n = 1 then x else x * pow(x, n-1)
+;;
+
+let rec pow(x, n) = (* x ^ n *)
+  if n = 0 then 1
+  else if n = 1 then x 
+  else pow(x * x, n / 2) * if even n then 1 else x
 ;;
