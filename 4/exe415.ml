a952b829f2db5c193f796ed1415a71ec98233403
DenTaku <dentaku1412@gmail.com>
2020-09-30 23:50:16 +0900

    Implement "integral" completely

--- 4/exe415.ml
+++ 4/exe415.ml
@@ -1,10 +1,17 @@
 (* 4.1 *)
 let integral (f, a, b) =
-  let d = 0.1e-10 in
+  let d = 0.1e-2 in
   let i = (b -. a) /. d in
   let rec sigma (f', i') = 
-    if i' = 0. then 0.
+    if 0. -. d <= i' && i' <= 0. +. d then 0.
     else (f'(a +. (i' -. 1.) *. d) +. f(a +. i' *. d)) *. d /. 2. 
       +. sigma(f', i' -. 1.) in
   sigma(f, i)
 ;;
+
+let pi = 3.14;;
+integral(sin, 0., pi);;
+
+(* 4.1 *)
+
+(* let pow x = fn n ->   *)
