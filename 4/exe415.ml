d0f99feb7f73138e9c63c6c1e77c7a51fc374aac
DenTaku <dentaku1412@gmail.com>
2020-10-01 00:59:49 +0900

    Make declearations' order consistent

--- 4/exe415.ml
+++ 4/exe415.ml
@@ -14,16 +14,16 @@ integral(sin, 0., pi);;
 
 (* 4.1 *)
 
-let rec pow'(x, n) = (* x ^ n *)
-  if n = 0 then 1
-  else if n = 1 then x 
-  else pow'(x * x, n / 2) * if even n then 1 else x
-;;
-
 let rec even n = (* works for positive intergers *)
   if n = 0 then true else odd(n-1)
 and odd n =
   if n = 0 then false else even(n-1)
+;;
+
+let rec pow'(x, n) = (* x ^ n *)
+  if n = 0 then 1
+  else if n = 1 then x 
+  else pow'(x * x, n / 2) * if even n then 1 else x
 ;;
 
 let pow x = fun n -> pow'(x, n);;
