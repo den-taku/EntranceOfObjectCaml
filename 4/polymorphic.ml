a89a5875f0fb06758f7a31ab7f3465390f02b5aa
DenTaku <dentaku1412@gmail.com>
2020-10-02 00:02:21 +0900

    Learn let poly. and value poly. (and η-expansion)

--- 4/polymorphic.ml
+++ 4/polymorphic.ml
@@ -7,3 +7,26 @@ let fst ((x, y) : 'a * 'b) = x;;
 let id x = x;;
 
 let apply f x = f x;;
+
+let double f x = f (f x) ;;
+
+double (fun x -> x + 1) 3;;
+
+double (fun s -> "<" ^ s ^ ">") "abc";;
+
+double double (fun x -> x + 1) 3;;
+
+double double (fun s -> "<" ^ s ^ ">") "abc";;
+
+let fourtimes = double double in
+  (fourtimes (fun x -> x + 1) 3);;
+
+let fourtimes = double double;;
+
+fourtimes (fun x -> x + 1) 3;;
+
+let fourtimes' f = double double f ;;
+(* equivalent to "let fourtimes' = fun f -> double double f" *)
+
+fourtimes' (fun x -> x + 1) 3;;
+fourtimes' (fun s -> "<" ^ s ^ ">") "abc" ;;
