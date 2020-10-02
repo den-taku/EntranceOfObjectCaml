c27aadad36240e5dce9e85e748232da9082f0613
DenTaku <dentaku1412@gmail.com>
2020-10-02 20:15:26 +0900

    Learn match and list

--- 5/list.ml
+++ 5/list.ml
@@ -12,3 +12,18 @@ let campuslist = "Yoshida" :: "Uji" :: "Katsura" :: [] ;;
 
 [(fun x -> x + 1); (fun x -> x * 2)] ;;
 [1; 2; 3] :: [[4; 5]; []; [6; 7; 8]] ;;
+
+(* let sum_list3 ([x; y; z]) = x + y + z ;; *)
+
+let rec sum_list l = 
+  match l with 
+    [] -> 0
+  | n :: rest -> n + (sum_list rest) 
+;;
+
+let rec max_list l =
+  match l with 
+    [x] -> x
+  | n1 :: n2 :: rest -> 
+    if n1 > n2 then max_list (n1 :: rest) else max_list (n2 :: rest)
+;;
