fe3e2249340d4727b6a3304d3dfb8635c87ec779
DenTaku <dentaku1412@gmail.com>
2020-10-02 15:19:34 +0900

    Finish all exercises

--- 4/combinator.ml
+++ 4/combinator.ml
@@ -12,3 +12,12 @@ let k x y = x;;
 let const17 = k 17 in const17 4.0;;
 
 let s x y z = x z (y z) ;;
+
+(* f $ f $ f $ f $ .... (n times) *)
+let rec funny f n =
+  if n = 0 then id
+  else if n mod 2 = 0 then funny (f $ f) (n / 2)
+  else funny (f $ f) (n / 2) $ f
+;;
+
+let swp x y = k y x;;
--- 4/exe424.ml
+++ 4/exe424.ml
@@ -12,3 +12,19 @@ let uncurry f (x, y) = f x y;;
 let avg = uncurry curried_avg in
   avg (4.0, 5.3)
 ;;
+
+let rec repeat f n x =
+  if n > 0 then repeat f (n-1) (f x) else x
+;;
+
+let k (x, y) = y;;
+
+let fib n = 
+  let (fibn, _) = repeat (fun (x, y) -> (x + y, x)) n (1, 0)
+  in fibn
+;;
+
+for i = 0 to 10 do 
+  print_int(fib i);
+  print_string "\n"
+done;;
