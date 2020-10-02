6458e9ab3dd7534702ed82ec32fa913b7486ce9b
DenTaku <dentaku1412@gmail.com>
2020-10-02 17:12:11 +0900

    Learn lists' basis

---
+++ 5/list.ml
@@ -0,0 +1,14 @@
+[3; 9; 0; -10];;
+
+let week = ["Sun"; "Mon"; "Tue"; "Wed"; "Thu"; "Fri"; "Sat"];;
+
+let oddnums = [3; 9; 253] ;;
+let more_oddnums = 99 :: oddnums ;;
+let evennums = 4 :: 10 :: [256; 12] ;;
+[];;
+
+let boollist = true :: false :: [] ;;
+let campuslist = "Yoshida" :: "Uji" :: "Katsura" :: [] ;;
+
+[(fun x -> x + 1); (fun x -> x * 2)] ;;
+[1; 2; 3] :: [[4; 5]; []; [6; 7; 8]] ;;
