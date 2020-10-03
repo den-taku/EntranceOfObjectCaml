9a6a87a8e7a7a84fe08c585db7eedfeb9b579c11
DenTaku <dentaku1412@gmail.com>
2020-10-03 15:04:54 +0900

    Learn variant more

--- 6/variant.ml
+++ 6/variant.ml
@@ -24,3 +24,18 @@ let rec map f = function
 ;;
 
 map area figs ;;
+
+let enclosing_square = function
+    Point -> Square 1
+  | Circle r -> Square (r * 2)
+  | Rectangle (_, l) | Square l -> Square l
+;;
+
+let similar x y =
+  match (x, y) with 
+      (Point, Point) | (Circle _, Circle _) | (Square _, Square _) -> true
+    | (Rectangle (l1, l2), Rectangle (l3, l4)) -> (l3 * l2 - l4 * l1) = 0
+    | _ -> false
+;;
+
+similar (Rectangle (2, 4)) (Rectangle (1, 2)) ;;
