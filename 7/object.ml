3b5f08a96136c48db777625452dcfa8c1108a3bc
DenTaku <dentaku1412@gmail.com>
2020-10-05 19:16:34 +0900

    Learn pseudo OOP in OCaml

--- 7/object.ml
+++ 7/object.ml
@@ -12,3 +12,17 @@ let p =
 p.get();;
 p.inc();;
 p.get();;
+
+let pointC x =
+  let rec this () =
+    { get = (fun() -> !x);
+      set = (fun newx -> x := newx);
+      inc = (fun () -> (this ()).set ((this ()).get () + 1))} in
+  this ()
+;;
+
+let new_point x = pointC (ref x) ;;
+
+let p = new_point 2 ;;
+
+p.inc() ; p.get() ;;
