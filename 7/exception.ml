51537a6bee64c38ed61cec57d231a7f04647d8a3
DenTaku <dentaku1412@gmail.com>
2020-10-05 20:43:37 +0900

    Learn exception declearation

--- 7/exception.ml
+++ 7/exception.ml
@@ -1,11 +1,130 @@
+exception Foo ;;
+
+(* raise Foo ;; *)
+
+let f () = if raise Foo then raise Foo else 3 ;;
+
+exception Bar of int ;;
+
+(* raise (Bar 2) ;; *)
+
+let exnlist = [Bar 2; Foo] ;;
+
+let f = function
+    Foo -> 0
+  | x -> raise x
+;;
+
+f Foo ;;
+
+f (Bar 4) ;;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 (* Exception: Match_failure ("//toplevel//", 1, 8). *)
-let f = function 
+(* let f = function 
   [] -> ()
 ;;
-f [2] ;;
+f [2] ;; *)
 
 (* Exception: Sys_error ": No such file or directory". *)
-open_in "" ;;
+(* open_in "" ;; *)
 
 (* Exception: Division_by_zero. *)
-print_string (string_of_int (4 / 0)) ;;
+(* print_string (string_of_int (4 / 0)) ;; *)
